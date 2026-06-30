c9f35444ec3d4488503a341e6943a35be6f2e0f7
Glenn Velasco <glennvelasco525@gmail.com>
2026-06-30 21:31:58 +0000

    feat(auth): Fortify config, actions, responses, and middleware

---
+++ app/Http/Responses/VerifyEmailResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Responses;
+
+use App\Enums\Role;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Laravel\Fortify\Contracts\VerifyEmailResponse as VerifyEmailResponseContract;
+
+class VerifyEmailResponse implements VerifyEmailResponseContract
+{
+    public function toResponse($request): JsonResponse|RedirectResponse
+    {
+        if ($request->wantsJson()) {
+            return new JsonResponse('', 204);
+        }
+
+        $user = $request->user();
+
+        return redirect()->intended(
+            ($user->hasRole(Role::Admin->value) ? route('admin.dashboard') : route('dashboard')).'?verified=1'
+        );
+    }
+}
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Enums\Role;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('admin-dashboard', function (User $user) {
+    return $user->hasRole(Role::Admin->value);
 });
