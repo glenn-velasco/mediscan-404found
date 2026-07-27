878886ce616a4af93a875ae357e187126b0c7a5e
Glenn Velasco <glennvelasco525@gmail.com>
2026-07-27 09:45:12 +0800

    fix(api): require all address fields in store and update medical information requests

--- app/Http/Requests/StoreMedicalInformationRequest.php
+++ app/Http/Requests/StoreMedicalInformationRequest.php
@@ -21,35 +21,38 @@ class StoreMedicalInformationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => ['required', 'string', 'max:255'],
-            'middle_name' => ['nullable', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'first_name' => ['required', 'string', 'trim', 'min:1', 'max:255'],
+            'middle_name' => ['nullable', 'string', 'trim', 'max:255'],
+            'last_name' => ['required', 'string', 'trim', 'min:1', 'max:255'],
             'suffix' => ['nullable', 'string', Rule::in(NameSuffix::values())],
 
-            'dob' => ['required', 'date', 'before:today'],
+            'date_of_birth' => ['required', 'date', 'date_format:Y-m-d', 'before:today'],
             'gender' => ['required', 'string', Rule::in(Gender::values())],
             'blood_type' => ['nullable', 'string', Rule::in(BloodType::values())],
             'religion' => ['nullable', 'string', Rule::in(Religion::values())],
-            'national_id' => ['nullable', 'string', 'max:255'],
 
-            'address.province' => ['nullable', 'string', 'max:255'],
-            'address.street' => ['nullable', 'string', 'max:255'],
-            'address.unit' => ['nullable', 'string', 'max:255'],
-            'address.country' => ['nullable', 'string', 'max:255'],
-            'address.postal_code' => ['nullable', 'string', 'max:20'],
-            'address.city' => ['nullable', 'string', 'max:255'],
+            'phone_country_code' => ['nullable', 'string', 'trim', 'max:5'],
+            'phone' => ['nullable', 'string', 'trim', 'regex:/^[\d\s\-+()]*$/', 'max:32'],
+
+            'address.province' => ['required', 'string', 'trim', 'max:255'],
+            'address.street' => ['required', 'string', 'trim', 'max:255'],
+            'address.unit' => ['required', 'string', 'trim', 'max:255'],
+            'address.country' => ['required', 'string', 'trim', 'max:255'],
+            'address.postal_code' => ['required', 'string', 'trim', 'max:20'],
+            'address.city' => ['required', 'string', 'trim', 'max:255'],
 
             'no_blood_transfusion' => ['sometimes', 'boolean'],
 
             'contacts' => ['sometimes', 'array'],
-            'contacts.*.name' => ['required_with:contacts', 'string', 'max:255'],
+            'contacts.*.name' => ['required_with:contacts', 'string', 'trim', 'min:1', 'max:255'],
             'contacts.*.relationship' => ['nullable', 'string', Rule::in(RelationToPatient::values())],
-            'contacts.*.phone_number' => ['required_with:contacts', 'string', 'max:32'],
-            'contacts.*.phone_country_code' => ['required_with:contacts', 'string', 'max:5'],
+            'contacts.*.phone_number' => ['required_with:contacts', 'string', 'trim', 'regex:/^[\d\s\-+()]*$/', 'max:32'],
+            'contacts.*.phone_country_code' => ['required_with:contacts', 'string', 'trim', 'max:5'],
             'contacts.*.is_primary' => ['sometimes', 'boolean'],
 
             'transfusion_consents' => ['sometimes', 'array'],
-            'transfusion_consents.*.consenter_name' => ['required_with:transfusion_consents', 'string', 'max:255'],
+            'transfusion_consents.*.consenter_name' => ['required_with:transfusion_consents', 'string', 'trim', 'min:1', 'max:255'],
             'transfusion_consents.*.relationship_to_patient' => ['nullable', 'string', Rule::in(RelationToPatient::values())],
             'transfusion_consents.*.consented_at' => ['required_with:transfusion_consents', 'date'],
         ];
